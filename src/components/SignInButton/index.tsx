e01eb1f8d077666a99a5cf28eb2e2c5b08432c1b
Suporte TI <suporte.zanaflex@ZANAN.LOCAL>
2023-04-13 17:51:04 -0300

    Fifth Commit

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,22 +2,26 @@ import styles from './styles.module.scss'
 
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession} from 'next-auth/react' 
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
         type="button"
-        className={styles.signInButton}>
+        className={styles.signInButton}
+        onClick={() => signOut()}>
             <FaGithub color='#04d361'/>
-            Vinicius Medeiros
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcons}/>
         </button>
     ) : (
         <button 
         type="button"
-        className={styles.signInButton}>
+        className={styles.signInButton}
+        onClick={() => signIn('github')}
+        >
             <FaGithub color='#eba417'/>
             Sign in with GitHub
         </button>
